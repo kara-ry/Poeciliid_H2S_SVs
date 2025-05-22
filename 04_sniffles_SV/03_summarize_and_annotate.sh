4fdeb33a76139c8fd9c27efc973b9c84fef90c59
Kara Ryan <71788965+kara-ry@users.noreply.github.com>
2025-05-22 13:24:17 -0700

    Update 03_summarize_and_annotate.sh

--- 04_sniffles_SV/03_summarize_and_annotate.sh
+++ 04_sniffles_SV/03_summarize_and_annotate.sh
@@ -3,3 +3,6 @@ cat your_file.vcf | grep -v '^#' | grep -o 'GENE=[^;]*' | sed 's/GENE=//' | sort
 
 bedtool intersect -a ${1}.vcf -b ${2}.bed > $3
 
+samtools view input.bam "NC_024348.1_RagTag:27837471-27944631" > output.bam
+
+samtools view input.bam "NC_024348.1_RagTag:27827471-27954631" > output.bam
